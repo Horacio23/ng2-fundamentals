7fea20fce2edadac2259d74e9847320f0878abe6
Horacio <horacio.delgado23@gmail.com>
2017-02-11 00:56:47 -0500

    added my first component

---
+++ app/events-app.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'events-app',
+    template: '<h2>Hello World</h2>'
+})
+export class EventsAppComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
