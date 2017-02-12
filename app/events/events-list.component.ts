ffb865708127f57d560458914d8bae20aafe9681
Horacio <horacio.delgado23@gmail.com>
2017-02-11 22:18:21 -0500

    added thumbnail component with input and output

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
     selector: 'events-list',
     template: `
         <div>
-            <h1>Upcoming Angular 2 Events</h1>
+            <h1> Upcoming Angular 2 Events </h1>
+            <hr/>
+            <event-thumbnail [event]="event" (eventClick)="handleEventClicked($event)"></event-thumbnail>
         </div> 
     `
 })
 export class EventsListComponent implements OnInit {
+    event = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '9/26/2036',
+        time: '10:30 am',
+        price: 599.99,
+        imageUrl: 'app/assets/images/angularconnect-shield.png',
+        location: {
+            address: '1057 DT',
+            city: 'London',
+            country: 'England'
+        }
+    }
+
+    handleEventClicked(data: string) {
+        console.log(data);
+    }
     constructor() { }
 
     ngOnInit() { }
