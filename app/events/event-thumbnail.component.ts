9bdc3a1296c2f0a5a934354f84d215866a33a8fd
Horacio <horacio.delgado23@gmail.com>
2017-02-15 16:03:24 -0500

    added safe navigation and ngIf for styling

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,17 +6,22 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     template: `
     <div>
         <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
-            <div>Price: \${{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            <h2>{{event?.name}}</h2>
+            <div>Date: {{event?.date}}</div>
+            <div>Time: {{event?.time}}</div>
+            <div>Price: \${{event?.price}}</div>
+            <div *ngIf="event.location">
+                <span>Location: {{event?.location?.address}}</span>
+                <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+            </div>
+            <div *ngIf="event.onlineUrl">
+                Online URL: {{event?.onlineUrl}}
+            </div>
         </div>
     </div>  
     `,
     styles: [`
+        .thumbnail {}
         .pad-left { margin-left: 10px;}
         .well div { color: #bbb;}
     `]
