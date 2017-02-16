1a48169612574b07e39e387ea22ad0ed08e648b7
Horacio <horacio.delgado23@gmail.com>
2017-02-16 16:58:27 -0500

    added the toastr service to add a popup

--- app/events-app.component.ts
+++ app/events-app.component.ts
@@ -6,12 +6,7 @@ import { Component, OnInit } from '@angular/core';
         <nav-bar></nav-bar>
         <events-list></events-list>
     `,
-    styles: [`
-       .nav.navbar-nav { font-size: 15px;} 
-       #searchForm { margin-right: 100px }
-       @media (max-width: 1200px ) {#searchForm {display:none}}
-    `]
-})
+    })
 export class EventsAppComponent implements OnInit {
     constructor() { }
 
--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service';
 
 @Component({
     selector: 'events-list',
@@ -9,20 +10,23 @@ import { EventService } from './shared/event.service';
             <hr/>
             <div class="row" >
                 <div class="col-md-5" *ngFor="let e of events">
-                    <event-thumbnail #thumbnail  (click)="handleThumbnailClick(e.name)" [event]="e"></event-thumbnail>
+                    <event-thumbnail #thumbnail (click)="handleThumbnailClick(e.name)" [event]="e"></event-thumbnail>
                 </div>
             </div>
         </div> 
     `
 })
 export class EventsListComponent implements OnInit {
-    // comment
     events: any[];
     
-    constructor(private _eventService: EventService){
+    constructor(private _eventService: EventService, private toastr: ToastrService){
     }
 
     ngOnInit() {
        this.events = this._eventService.getEvents(); 
+    }
+
+    handleThumbnailClick(eventName){
+        this.toastr.success(eventName);
     }
 }
