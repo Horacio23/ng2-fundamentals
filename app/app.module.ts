125b4e414e92c9df9c49351b1dc7e6b15f1281be
Horacio <horacio.delgado23@gmail.com>
2017-02-17 09:48:11 -0500

    added event-details component to the app

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent }   from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
@@ -16,6 +17,7 @@ import { ToastrService } from './common/toastr.service';
         EventsListComponent,
         EventThumbnailComponent, 
         NavBarComponent,
+        EventDetailsComponent,
     ],
     providers: [
         EventService,
